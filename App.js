982e5aa2ea36886986e310d85fd33b561136dbbb
Andrew <andrewpurves8@gmail.com>
2021-08-25 18:02:39 +0200

    Initial commit

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.gameArea}>
+        <View style={styles.row}>
+          <View styles={styles.tile}>
+            <Text>asdasd</Text>
+          </View>
+          <View styles={styles.tile}>
+            <Text>zxczxc</Text>
+          </View>
+          <View styles={styles.tile}>
+            <Text>dfgdfgdf</Text>
+          </View>
+          <View styles={styles.tile}>
+            <Text>dfgdsf</Text>
+          </View>
+        </View>
+        <View style={styles.row}>
+          <View styles={styles.tile}>
+            <Text>asdasd</Text>
+          </View>
+          <View styles={styles.tile}>
+            <Text>zxczxc</Text>
+          </View>
+          <View styles={styles.tile}>
+            <Text>dfgdfgdf</Text>
+          </View>
+          <View styles={styles.tile}>
+            <Text>dfgdsf</Text>
+          </View>
+        </View>
+        <View style={styles.row}></View>
+        <View style={styles.row}></View>
+      </View>
     </View>
   );
 }
@@ -14,8 +43,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  gameArea: {
+    backgroundColor: "#a9a9a9",
+    alignItems: "stretch",
+    justifyContent: "flex-start",
+    width: "90%",
+    aspectRatio: 1,
+  },
+  row: {
+    flex: 1,
+    backgroundColor: "#a9a9a9",
+    alignItems: "stretch",
+    justifyContent: "space-around",
+    flexDirection: "row",
+  },
+  tile: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
