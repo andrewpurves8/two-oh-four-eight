b39b10f51078a657bad3750d41acd82bdcf8e939
Andrew <andrewpurves8@gmail.com>
2021-08-29 17:13:12 +0200

    First draft of animations

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,24 +1,84 @@
-import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import React, { Component, useEffect, useRef } from "react";
+import { Animated, StyleSheet, View, Text } from "react-native";
 
 import Colors from "./Colors";
 
+function AnimView(props) {
+  const anim = new Animated.Value(0);
+
+  Animated.timing(anim, {
+    toValue: 1,
+    duration: 200,
+    useNativeDriver: true,
+  }).start();
+
+  return (
+    <Animated.View
+      style={{
+        ...props.style,
+        opacity: props.isNew ? anim : 1,
+        transform: [
+          {
+            translateX: anim.interpolate({
+              inputRange: [0, 1],
+              outputRange: [
+                77 * props.oldCol + 8.4 * (props.oldCol + 1),
+                77 * props.col + 8.4 * (props.col + 1),
+              ],
+            }),
+          },
+          {
+            translateY: anim.interpolate({
+              inputRange: [0, 1],
+              outputRange: [
+                77 * props.oldRow + 8.4 * (props.oldRow + 1),
+                77 * props.row + 8.4 * (props.row + 1),
+              ],
+            }),
+          },
+        ],
+      }}
+    >
+      {props.children}
+    </Animated.View>
+  );
+}
+
 function Tile(props) {
+  const value = props.value;
+  const oldRow = props.row;
+  const oldCol = props.col;
+  let row = props.row;
+  let col = props.col;
+  if (value != 0) {
+    row = props.newRow;
+    col = props.newCol;
+  }
+  const isNew = props.isNew;
+
   let bgColor = Colors.blankTile;
 
-  if (props.value !== 0) {
-    const valueLog = Math.log2(props.value);
+  if (value !== 0) {
+    const valueLog = Math.log2(value);
     bgColor = Colors.tileColors[valueLog - 1];
   }
 
-  const textColor = props.value > 4 ? Colors.lightText : Colors.darkText;
+  const textColor = value > 4 ? Colors.lightText : Colors.darkText;
 
   return (
-    <View style={[styles.tile, { backgroundColor: bgColor }]}>
-      <Text style={[styles.text, { color: textColor }]}>
-        {props.value === 0 ? "" : props.value}
+    <AnimView
+      style={{ ...styles.tile, backgroundColor: bgColor }}
+      oldRow={oldRow}
+      row={row}
+      oldCol={oldCol}
+      col={col}
+      value={value}
+      isNew={isNew}
+    >
+      <Text style={{ ...styles.text, color: textColor }}>
+        {value === 0 ? "" : value}
       </Text>
-    </View>
+    </AnimView>
   );
 }
 
@@ -32,8 +92,9 @@ const styles = StyleSheet.create({
   tile: {
     alignItems: "center",
     borderRadius: 5,
-    height: "100%",
+    height: 77,
     justifyContent: "center",
-    width: "22%",
+    position: "absolute",
+    width: 77,
   },
 });
--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -17,17 +17,39 @@ class MainScreen extends Component {
       [0, 0, 0, 0],
       [0, 0, 0, 0],
     ],
+    nonZeroTiles: [],
   };
 
   constructor(props) {
     super(props);
     let newValues = this.state.values;
+    let newNonZeroTiles = this.state.nonZeroTiles;
 
-    for (let i = 0; i < 2; i++) this.addTile(newValues);
+    for (let i = 0; i < 2; i++) this.addTile(newValues, newNonZeroTiles);
+  }
 
-    let newState = this.state;
-    newState.values = newValues;
-    this.state = newState;
+  addTile(values, nonZeroTiles) {
+    let index;
+    let row;
+    let col;
+    do {
+      index = Math.floor(Math.random() * 16);
+      row = Math.floor(index / 4);
+      col = index % 4;
+    } while (values[row][col] != 0);
+
+    const value = Math.floor(Math.random() * 2 + 1) * 2;
+    values[row][col] = value;
+    const tile = {
+      index: 4 * row + col,
+      row: row,
+      col: col,
+      value: value,
+      newRow: row,
+      newCol: col,
+      isNew: true,
+    };
+    nonZeroTiles.push(tile);
   }
 
   checkGameLost(values) {
@@ -61,94 +83,208 @@ class MainScreen extends Component {
     return false;
   }
 
-  addTile(values) {
-    let index;
-    let row;
-    let col;
-
-    do {
-      index = Math.floor(Math.random() * 16);
-      row = Math.floor(index / 4);
-      col = index % 4;
-    } while (values[row][col] != 0);
-
-    values[row][col] = Math.floor(Math.random() * 2 + 1) * 2;
-  }
-
-  postUpdate(newValues, oldValues) {
+  postUpdate(newValues, oldValues, nonZeroTiles) {
     if (JSON.stringify(newValues) != JSON.stringify(oldValues)) {
       this.checkGameWon(newValues);
-      this.addTile(newValues);
+      this.addTile(newValues, nonZeroTiles);
       this.checkGameLost(newValues);
-
-      let newState = this.state;
-      newState.values = newValues;
-
-      this.setState(newState);
+      this.setState({
+        ...this.state,
+        values: newValues,
+        nonZeroTiles: nonZeroTiles,
+      });
     }
   }
 
   onSwipeUp(gestureState) {
+    // let newValues = _.cloneDeep(this.state.values);
+    // for (let i = 0; i < 4; i++) {
+    //   for (let j = 0; j < 3; j++) {
+    //     this.shiftUp(newValues, i);
+    //     if (newValues[j][i] === newValues[j + 1][i]) {
+    //       newValues[j][i] *= 2;
+    //       newValues[j + 1][i] = 0;
+    //     }
+    //   }
+    // }
+    // this.postUpdate(newValues, this.state.values);
+
     let newValues = _.cloneDeep(this.state.values);
+    let newNonZeroTiles = _.cloneDeep(this.state.nonZeroTiles);
+
+    for (let i = 0; i < newNonZeroTiles.length; i++) {
+      let tile = newNonZeroTiles[i];
+      tile.row = tile.newRow;
+      tile.col = tile.newCol;
+      tile.isNew = false;
+    }
+
     for (let i = 0; i < 4; i++) {
       for (let j = 0; j < 3; j++) {
-        this.shiftUp(newValues, i);
-        if (newValues[j][i] === newValues[j + 1][i]) {
+        this.shiftUp(newValues, i, newNonZeroTiles);
+        if (newValues[j][i] !== 0 && newValues[j][i] === newValues[j + 1][i]) {
           newValues[j][i] *= 2;
           newValues[j + 1][i] = 0;
+          let topTile, bottomTile;
+          for (let k = 0; k < newNonZeroTiles.length; k++) {
+            if (newNonZeroTiles[k].index === 4 * (j + 1) + i) {
+              bottomTile = newNonZeroTiles[k];
+            } else if (newNonZeroTiles[k].index === 4 * j + i) {
+              topTile = newNonZeroTiles[k];
+            }
+          }
+          if (typeof bottomTile != "undefined") {
+            bottomTile.index = 4 * i + j;
+            bottomTile.value *= 2;
+            bottomTile.newRow = j;
+          }
+          const topTileIndex = newNonZeroTiles.indexOf(topTile);
+          if (topTileIndex > -1) newNonZeroTiles.splice(topTileIndex, 1);
         }
       }
     }
-
-    this.postUpdate(newValues, this.state.values);
+    this.postUpdate(newValues, this.state.values, newNonZeroTiles);
   }
 
   onSwipeDown(gestureState) {
+    // let newValues = _.cloneDeep(this.state.values);
+    // for (let i = 0; i < 4; i++) {
+    //   for (let j = 3; j > 0; j--) {
+    //     this.shiftDown(newValues, i);
+    //     if (newValues[j][i] === newValues[j - 1][i]) {
+    //       newValues[j][i] *= 2;
+    //       newValues[j - 1][i] = 0;
+    //     }
+    //   }
+    // }
+    // this.postUpdate(newValues, this.state.values);
+
     let newValues = _.cloneDeep(this.state.values);
+    let newNonZeroTiles = _.cloneDeep(this.state.nonZeroTiles);
+
+    for (let i = 0; i < newNonZeroTiles.length; i++) {
+      let tile = newNonZeroTiles[i];
+      tile.row = tile.newRow;
+      tile.col = tile.newCol;
+      tile.isNew = false;
+    }
+
     for (let i = 0; i < 4; i++) {
       for (let j = 3; j > 0; j--) {
-        this.shiftDown(newValues, i);
-        if (newValues[j][i] === newValues[j - 1][i]) {
+        this.shiftDown(newValues, i, newNonZeroTiles);
+        if (newValues[j][i] !== 0 && newValues[j][i] === newValues[j - 1][i]) {
           newValues[j][i] *= 2;
           newValues[j - 1][i] = 0;
+          let topTile, bottomTile;
+          for (let k = 0; k < newNonZeroTiles.length; k++) {
+            if (newNonZeroTiles[k].index === 4 * (j - 1) + i) {
+              topTile = newNonZeroTiles[k];
+            } else if (newNonZeroTiles[k].index === 4 * j + i) {
+              bottomTile = newNonZeroTiles[k];
+            }
+          }
+          if (typeof topTile != "undefined") {
+            topTile.index = 4 * j + i;
+            topTile.value *= 2;
+            topTile.newRow = j;
+          }
+          const bottomTileIndex = newNonZeroTiles.indexOf(bottomTile);
+          if (bottomTileIndex > -1) newNonZeroTiles.splice(bottomTileIndex, 1);
         }
       }
     }
-
-    this.postUpdate(newValues, this.state.values);
+    this.postUpdate(newValues, this.state.values, newNonZeroTiles);
   }
 
   onSwipeLeft(gestureState) {
+    // let newValues = _.cloneDeep(this.state.values);
+    // for (let i = 0; i < 4; i++) {
+    //   for (let j = 0; j < 3; j++) {
+    //     this.shiftLeft(newValues, i);
+    //     if (newValues[i][j] === newValues[i][j + 1]) {
+    //       newValues[i][j] *= 2;
+    //       newValues[i][j + 1] = 0;
+    //     }
+    //   }
+    // }
+    // this.postUpdate(newValues, this.state.values);
+
     let newValues = _.cloneDeep(this.state.values);
+    let newNonZeroTiles = _.cloneDeep(this.state.nonZeroTiles);
+
+    for (let i = 0; i < newNonZeroTiles.length; i++) {
+      let tile = newNonZeroTiles[i];
+      tile.row = tile.newRow;
+      tile.col = tile.newCol;
+      tile.isNew = false;
+    }
+
     for (let i = 0; i < 4; i++) {
       for (let j = 0; j < 3; j++) {
-        this.shiftLeft(newValues, i);
-        if (newValues[i][j] === newValues[i][j + 1]) {
+        this.shiftLeft(newValues, i, newNonZeroTiles);
+        if (newValues[i][j] !== 0 && newValues[i][j] === newValues[i][j + 1]) {
           newValues[i][j] *= 2;
           newValues[i][j + 1] = 0;
+          let leftTile, rightTile;
+          for (let k = 0; k < newNonZeroTiles.length; k++) {
+            if (newNonZeroTiles[k].index === 4 * i + j + 1) {
+              rightTile = newNonZeroTiles[k];
+            } else if (newNonZeroTiles[k].index === 4 * i + j) {
+              leftTile = newNonZeroTiles[k];
+            }
+          }
+          if (typeof rightTile != "undefined") {
+            rightTile.index = 4 * i + j;
+            rightTile.value *= 2;
+            rightTile.newCol = j;
+          }
+          const leftTileIndex = newNonZeroTiles.indexOf(leftTile);
+          if (leftTileIndex > -1) newNonZeroTiles.splice(leftTileIndex, 1);
         }
       }
     }
-
-    this.postUpdate(newValues, this.state.values);
+    this.postUpdate(newValues, this.state.values, newNonZeroTiles);
   }
 
   onSwipeRight(gestureState) {
     let newValues = _.cloneDeep(this.state.values);
+    let newNonZeroTiles = _.cloneDeep(this.state.nonZeroTiles);
+
+    for (let i = 0; i < newNonZeroTiles.length; i++) {
+      let tile = newNonZeroTiles[i];
+      tile.row = tile.newRow;
+      tile.col = tile.newCol;
+      tile.isNew = false;
+    }
+
     for (let i = 0; i < 4; i++) {
       for (let j = 3; j > 0; j--) {
-        this.shiftRight(newValues, i);
-        if (newValues[i][j] === newValues[i][j - 1]) {
+        this.shiftRight(newValues, i, newNonZeroTiles);
+        if (newValues[i][j] !== 0 && newValues[i][j] === newValues[i][j - 1]) {
           newValues[i][j] *= 2;
           newValues[i][j - 1] = 0;
+          let leftTile, rightTile;
+          for (let k = 0; k < newNonZeroTiles.length; k++) {
+            if (newNonZeroTiles[k].index === 4 * i + j - 1) {
+              leftTile = newNonZeroTiles[k];
+            } else if (newNonZeroTiles[k].index === 4 * i + j) {
+              rightTile = newNonZeroTiles[k];
+            }
+          }
+          if (typeof leftTile != "undefined") {
+            leftTile.index = 4 * i + j;
+            leftTile.value *= 2;
+            leftTile.newCol = j;
+          }
+          const rightTileIndex = newNonZeroTiles.indexOf(rightTile);
+          if (rightTileIndex > -1) newNonZeroTiles.splice(rightTileIndex, 1);
         }
       }
     }
-
-    this.postUpdate(newValues, this.state.values);
+    this.postUpdate(newValues, this.state.values, newNonZeroTiles);
   }
 
-  shiftUp(values, column) {
+  shiftUp(values, column, nonZeroTiles) {
     for (let i = 0; i < 3; i++) {
       if (values[i][column] != 0) continue;
 
@@ -156,13 +292,24 @@ class MainScreen extends Component {
       while (j < 3 - i && values[i + j][column] == 0) j++;
 
       if (values[i + j][column] != 0) {
-        values[i][column] = values[i + j][column];
-        values[i + j][column] = 0;
+        let tile;
+        for (let k = 0; k < nonZeroTiles.length; k++) {
+          if (nonZeroTiles[k].index === 4 * (i + j) + column) {
+            tile = nonZeroTiles[k];
+            break;
+          }
+        }
+        if (typeof tile != "undefined") {
+          tile.newRow = i;
+          tile.index = 4 * i + column;
+          values[i][column] = values[i + j][column];
+          values[i + j][column] = 0;
+        }
       }
     }
   }
 
-  shiftDown(values, column) {
+  shiftDown(values, column, nonZeroTiles) {
     for (let i = 3; i > 0; i--) {
       if (values[i][column] != 0) continue;
 
@@ -170,13 +317,24 @@ class MainScreen extends Component {
       while (j < i && values[i - j][column] == 0) j++;
 
       if (values[i - j][column] != 0) {
-        values[i][column] = values[i - j][column];
-        values[i - j][column] = 0;
+        let tile;
+        for (let k = 0; k < nonZeroTiles.length; k++) {
+          if (nonZeroTiles[k].index === 4 * (i - j) + column) {
+            tile = nonZeroTiles[k];
+            break;
+          }
+        }
+        if (typeof tile != "undefined") {
+          tile.newRow = i;
+          tile.index = 4 * i + column;
+          values[i][column] = values[i - j][column];
+          values[i - j][column] = 0;
+        }
       }
     }
   }
 
-  shiftLeft(values, row) {
+  shiftLeft(values, row, nonZeroTiles) {
     for (let i = 0; i < 3; i++) {
       if (values[row][i] != 0) continue;
 
@@ -184,13 +342,24 @@ class MainScreen extends Component {
       while (j < 3 - i && values[row][i + j] == 0) j++;
 
       if (values[row][i + j] != 0) {
-        values[row][i] = values[row][i + j];
-        values[row][i + j] = 0;
+        let tile;
+        for (let k = 0; k < nonZeroTiles.length; k++) {
+          if (nonZeroTiles[k].index === 4 * row + i + j) {
+            tile = nonZeroTiles[k];
+            break;
+          }
+        }
+        if (typeof tile != "undefined") {
+          tile.newCol = i;
+          tile.index = 4 * row + i;
+          values[row][i] = values[row][i + j];
+          values[row][i + j] = 0;
+        }
       }
     }
   }
 
-  shiftRight(values, row) {
+  shiftRight(values, row, nonZeroTiles) {
     for (let i = 3; i > 0; i--) {
       if (values[row][i] != 0) continue;
 
@@ -198,15 +367,25 @@ class MainScreen extends Component {
       while (j < i && values[row][i - j] == 0) j++;
 
       if (values[row][i - j] != 0) {
-        values[row][i] = values[row][i - j];
-        values[row][i - j] = 0;
+        let tile;
+        for (let k = 0; k < nonZeroTiles.length; k++) {
+          if (nonZeroTiles[k].index === 4 * row + i - j) {
+            tile = nonZeroTiles[k];
+            break;
+          }
+        }
+        if (typeof tile != "undefined") {
+          tile.newCol = i;
+          tile.index = 4 * row + i;
+          values[row][i] = values[row][i - j];
+          values[row][i - j] = 0;
+        }
       }
     }
   }
 
   render() {
     const config = {
-      // velocityThreshold: 0.3,
       velocityThreshold: 0.1,
       directionalOffsetThreshold: 80,
     };
@@ -228,23 +407,30 @@ class MainScreen extends Component {
   }
 
   createTiles() {
-    let rows = [];
+    let tiles = [];
 
     for (let i = 0; i < 4; i++) {
-      let tiles = [];
-
       for (let j = 0; j < 4; j++) {
-        tiles.push(<Tile value={this.state.values[i][j]} key={4 * i + j} />);
+        tiles.push(<Tile value={0} key={4 * i + j} row={i} col={j} />);
       }
+    }
 
-      rows.push(
-        <View style={styles.row} key={i + 16}>
-          {tiles}
-        </View>
+    for (let i = 0; i < this.state.nonZeroTiles.length; i++) {
+      const tile = this.state.nonZeroTiles[i];
+      tiles.push(
+        <Tile
+          value={tile.value}
+          row={tile.row}
+          col={tile.col}
+          newRow={tile.newRow}
+          newCol={tile.newCol}
+          isNew={tile.isNew}
+          key={16 + tile.index}
+        />
       );
     }
 
-    return rows;
+    return tiles;
   }
 }
 
@@ -258,22 +444,13 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   gameArea: {
-    alignItems: "stretch",
     aspectRatio: 1,
     backgroundColor: Colors.gameBackGround,
     borderRadius: 5,
-    flexDirection: "column",
-    justifyContent: "space-evenly",
-    width: "90%",
+    width: 350,
   },
   gestureRecognizer: {
     height: "100%",
     width: "100%",
-  },
-  row: {
-    alignItems: "center",
-    flexDirection: "row",
-    height: "22%",
-    justifyContent: "space-evenly",
   },
 });
